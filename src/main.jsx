2d785582dbf8252d1a344dfbfd49396c47dab8ed
bruce <hamzabarhrhouj@gmail.com>
2024-12-29 20:52:20 +0100

    field notes challenge

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import "./index.css";
 import App from "./App.jsx";
 
 import TextInput from "./TextInput.jsx";
+import VideoPlayer from "./VideoPlayer.jsx";
+import FieldNotes from "./FieldNotes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
     ##############################
     <TextInput />
+    <VideoPlayer />
+    <FieldNotes />
   </StrictMode>
 );
